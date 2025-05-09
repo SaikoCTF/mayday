8ce0fe4da5ca16dde1ca1ca263760204bcef6ebb
jimklo <jimklo@users.noreply.github.com>
2025-05-09 13:15:33 +0000

    deploy: 8fea0a519b2afcc1af8df5b3e8e08451e149683e

--- leaderboard/assets/scripts/data.js
+++ leaderboard/assets/scripts/data.js
@@ -55,73 +55,9 @@ data = {
     "participants": [
 
 {
-    "id": "P15761",
-    "handle": "SaltyHealer",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:03:13.837603"
-        },
-        {
-            "id": 3,
-            "time": "0:15:03.578132"
-        },
-        {
-            "id": 2,
-            "time": "0:01:01.497501"
-        },
-        {
-            "id": 4,
-            "time": "0:07:11.316257"
-        }
-    ]
-},
-{
-    "id": "P19871",
-    "handle": "BashfulSamurai",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:08:14.005622"
-        },
-        {
-            "id": 3,
-            "time": "0:49:53.286428"
-        },
-        {
-            "id": 2,
-            "time": "0:06:37.971740"
-        }
-    ]
-},
-{
-    "id": "P20335",
-    "handle": "WanderingInfiltrator",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:56:27.051881"
-        }
-    ]
-},
-{
-    "id": "P26631",
-    "handle": "ThunderingViking",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:25:57.108959"
-        }
-    ]
-},
-{
     "id": "P39357",
     "handle": "EquableLegionnaire",
     "challenges": [
-        {
-            "id": 1,
-            "time": "0:51:42.094444"
-        },
         {
             "id": 2,
             "time": "0:04:09.652161"
@@ -129,34 +65,12 @@ data = {
     ]
 },
 {
-    "id": "P41686",
-    "handle": "GoldenSamurai",
+    "id": "P42357",
+    "handle": "Unknown",
     "challenges": [
         {
             "id": 1,
-            "time": "0:07:44.616687"
-        }
-    ]
-},
-{
-    "id": "P49473",
-    "handle": "GreatBrawler",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:09:50.507301"
-        },
-        {
-            "id": 2,
-            "time": "0:03:20.420789"
-        },
-        {
-            "id": 5,
-            "time": "0:02:58.897507"
-        },
-        {
-            "id": 4,
-            "time": "0:03:07.848847"
+            "time": "1:00:00.000000"
         }
     ]
 },
@@ -165,34 +79,8 @@ data = {
     "handle": "FamousSpellthief",
     "challenges": [
         {
-            "id": 1,
-            "time": "0:04:49.900291"
-        },
-        {
-            "id": 3,
-            "time": "0:13:37.741096"
-        },
-        {
-            "id": 2,
-            "time": "0:09:07.599391"
-        },
-        {
-            "id": 4,
-            "time": "0:05:14.404974"
-        }
-    ]
-},
-{
-    "id": "P55672",
-    "handle": "IndustriousCleric",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:10:23.284907"
-        },
-        {
-            "id": 2,
-            "time": "0:01:22.695179"
+            "id": 5,
+            "time": "0:13:35.192683"
         }
     ]
 },
@@ -201,154 +89,41 @@ data = {
     "handle": "ImperfectSamurai",
     "challenges": [
         {
-            "id": 1,
-            "time": "0:32:03.311347"
-        },
-        {
-            "id": 3,
-            "time": "0:44:10.357654"
-        },
-        {
-            "id": 2,
-            "time": "0:03:28.212855"
-        },
-        {
             "id": 5,
             "time": "0:03:48.301803"
-        },
-        {
-            "id": 4,
-            "time": "0:05:52.843184"
         }
     ]
 },
 {
-    "id": "P57701",
-    "handle": "DeliriousBerserker",
+    "id": "P57660",
+    "handle": "Unknown",
     "challenges": [
         {
-            "id": 6,
-            "time": "0:06:45.930903"
-        },
-        {
             "id": 1,
-            "time": "0:30:46.312030"
-        },
-        {
-            "id": 3,
-            "time": "0:07:42.927251"
-        },
-        {
-            "id": 2,
-            "time": "0:01:41.476916"
-        },
-        {
-            "id": 5,
-            "time": "0:06:51.897544"
-        },
-        {
-            "id": 4,
-            "time": "0:04:00.936139"
+            "time": "0:04:33.958123"
         }
     ]
 },
 {
-    "id": "P65478",
-    "handle": "InquisitiveKristomancer",
+    "id": "P72470",
+    "handle": "Unknown",
     "challenges": []
 },
 {
-    "id": "P67269",
-    "handle": "FierceMercenary",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:46:02.738753"
-        },
-        {
-            "id": 3,
-            "time": "0:06:02.566842"
-        },
-        {
-            "id": 2,
-            "time": "0:04:32.808540"
-        },
-        {
-            "id": 4,
-            "time": "0:30:36.335354"
-        }
-    ]
+    "id": "P76075",
+    "handle": "Unknown",
+    "challenges": []
 },
+,
 {
-    "id": "P68731",
-    "handle": "ComplexSamurai",
+    "id": "P89296",
+    "handle": "Unknown",
     "challenges": []
 },
 {
-    "id": "P82317",
-    "handle": "ColossalSoulguard",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:20:24.328869"
-        }
-    ]
-},
-{
-    "id": "P91107",
-    "handle": "SneakyPriest",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:27:15.721141"
-        },
-        {
-            "id": 3,
-            "time": "0:37:07.322734"
-        },
-        {
-            "id": 2,
-            "time": "0:01:55.830350"
-        },
-        {
-            "id": 4,
-            "time": "0:12:53.444115"
-        }
-    ]
-},
-{
-    "id": "P94283",
-    "handle": "WoodenHarbinger",
-    "challenges": [
-        {
-            "id": 2,
-            "time": "0:12:11.814308"
-        }
-    ]
-},
-{
-    "id": "P95416",
-    "handle": "EarthyOutrider",
+    "id": "P92746",
+    "handle": "Unknown",
     "challenges": []
-},
-{
-    "id": "P97663",
-    "handle": "EquableChameleon",
-    "challenges": []
-},
-{
-    "id": "P99960",
-    "handle": "BashfulSoulguard",
-    "challenges": [
-        {
-            "id": 1,
-            "time": "0:43:25.766193"
-        },
-        {
-            "id": 2,
-            "time": "0:01:51.985279"
-        }
-    ]
 },
     ]
 }
